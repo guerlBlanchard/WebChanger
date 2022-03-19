fbfa67cfff097ad0a24e383948024165286a7fcc
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2022-03-19 23:27:07 +0100

    [Add] improved login menu

--- WebApp/Front/React/src/Login/Login.js
+++ WebApp/Front/React/src/Login/Login.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './Login.css';
 
 export default class Login extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className='Login'>
         <header className='Login-header'>
-          <box className='Login-box'>
-            <h1>
-              Login
-            </h1>
-          </box>
+          <div className='Login-box'>
+            <h1>Login</h1>
+            <form>
+              <label>Username</label>
+              <input type="text" placeholder='Username'/>
+              <label>Password</label>
+              <input type="password" placeholder='Password'/>
+            </form>
+          </div>
         </header>
       </div>
     );
