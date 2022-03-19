3da3ff8b20966a3e136c44b49a325483ca981c54
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2022-03-19 21:58:38 +0100

    [Del] removed the profiles in the docker compose [Add] start of the react login page

---
+++ WebApp/Front/React/src/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <div className='Login'>
+        <header className='Login-header'>
+          <box className='Login-box'>
+            <h1>
+              Login
+            </h1>
+          </box>
+        </header>
+      </div>
+    );
+  }
+}
