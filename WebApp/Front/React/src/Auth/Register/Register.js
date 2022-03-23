c8e35622110d35d58a5abe8e376fa38c435715b7
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2022-03-23 20:31:05 +0100

    [Add] saved input from the register form

--- WebApp/Front/React/src/Auth/Register/Register.js
+++ WebApp/Front/React/src/Auth/Register/Register.js
@@ -4,6 +4,18 @@ import './Register.css';
 export default class Register extends React.Component{
   constructor (props) {
     super(props);
+    this.state = {
+      mail: '',
+      phone: '',
+      username: '',
+      password: '',
+      cpassword: ''
+    }
+    this.SubmitRegister = this.SubmitRegister.bind(this);
+  }
+
+  SubmitRegister(event) {
+
   }
 
   render() {
@@ -13,11 +25,11 @@ export default class Register extends React.Component{
           <div className='Register-box'>
             <h1>Register</h1>
             <form className="Register-form">
-              <input type='text' placeholder="Email"/>
-              <input type='text' placeholder="Phone (optionnal)"/>
-              <input type="text" placeholder='Username'/>
-              <input type="password" placeholder='Password'/>
-              <input type="password" placeholder="Confirm Password"/>
+              <input type='text' placeholder="Email" value={this.state.mail} onChange={(event) => {this.setState({mail: event.target.mail})}}/>
+              <input type='text' placeholder="Phone (optionnal)" value={this.state.phone}  onChange={(event) => {this.setState({phone: event.target.phone})}}/>
+              <input type="text" placeholder='Username' value={this.state.username}  onChange={(event) => {this.setState({username: event.target.username})}}/>
+              <input type="password" placeholder='Password' value={this.state.password}  onChange={(event) => {this.setState({password: event.target.password})}}/>
+              <input type="password" placeholder="Confirm Password" value={this.state.cpassword}  onChange={(event) => {this.setState({cpassword: event.target.cpassword})}}/>
               <input type="submit" value="Register"/>
             </form>
           </div>
