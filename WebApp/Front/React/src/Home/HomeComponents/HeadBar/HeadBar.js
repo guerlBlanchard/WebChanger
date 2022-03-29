df0b0772172e42591071769ad5b3b8a52b797931
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2022-03-29 17:26:26 +0200

    [Add] funtionnal login and register button

--- WebApp/Front/React/src/Home/HomeComponents/HeadBar/HeadBar.js
+++ WebApp/Front/React/src/Home/HomeComponents/HeadBar/HeadBar.js
@@ -2,19 +2,16 @@ import React from "react";
 import './HeadBar.css';
 
 const HeadBar = () => {
-    return (
-        <div className="HeadBar">
-            <div className="HeadBar-index">
-
-            </div>
-            <div className="HeadBar-account">
-                <a href="">
-                    <button>Login</button>
-                </a>
-                <button>Register</button>
-            </div>
+  return (
+      <div className="HeadBar">
+        <div className="HeadBar-index">
         </div>
-    )
+        <div className="HeadBar-account">
+          <a href="http://localhost:3000/Login"><button>Login</button></a>
+          <a href="http://localhost:3000/Register"><button>Register</button></a>
+        </div>
+      </div>
+  )
 }
 
 export default HeadBar;
