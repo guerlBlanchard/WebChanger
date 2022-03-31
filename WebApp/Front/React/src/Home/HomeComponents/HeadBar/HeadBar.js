5875ae9fa32a1406e64e7cd0d80c9069927ddb40
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2022-03-31 23:06:28 +0200

    [Add] react icon package [Edit] reworked a bit on the headbar

--- WebApp/Front/React/src/Home/HomeComponents/HeadBar/HeadBar.js
+++ WebApp/Front/React/src/Home/HomeComponents/HeadBar/HeadBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './HeadBar.css';
+import {FaHome} from 'react-icons/fa';
 
 function HeadBar() {
   return (
       <div className="HeadBar">
         <div className="HeadBar-index">
+          <FaHome className="homeIcon"/>
         </div>
         <div className="HeadBar-account">
           <a href="http://localhost:3000/Login"><button>Login</button></a>
